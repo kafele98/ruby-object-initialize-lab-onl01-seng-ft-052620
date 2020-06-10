15570fd911b88b7418fad2764712e188a7efa575
Zoe Keller <keller.zoe98@gmail.com>
2020-06-10 19:47:44 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,18 @@
-class Dog 
-  attr_accessor :name 
-  attr_accessor :breed 
+class Dog
+  def initialize (dogs_name, dogs_breed = "Mutt")
+    @name = dogs_name
+    @breed = dogs_breed
+  end
+
+  def name=(dogs_name, dogs_breed = "Mutt")
+    @name = dogs_name
+    @breed = dogs_breed
+  end
+
+  def name
+    @name
+  end
+  def breed
+    @breed
+  end 
 end 
